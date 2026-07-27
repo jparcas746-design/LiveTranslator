2f01317b8f1567807080159b88a23740bdef3da8
Joaquín Paredes <jparcas746@gmail.com>
2026-07-27 20:57:08 +0200

    Probar ruta API

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -1,25 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-
-    console.log("RECIBIDO:", body);
-
-    return NextResponse.json({
-      response: "ThorAI funciona correctamente ⚡",
-    });
-
-  } catch (error) {
-    console.error("ERROR API:", error);
-
-    return NextResponse.json(
-      {
-        error: "Fallo en la API",
-      },
-      {
-        status: 500,
-      }
-    );
-  }
+export async function POST() {
+  return NextResponse.json({
+    response: "API funcionando ⚡",
+  });
 }
